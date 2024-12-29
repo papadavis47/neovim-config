c1cb435c9eda23785d1e34b276af9cb85ff1d709
John William Davis <jwdavisdeveloper@gmail.com>
2024-12-29 11:28:23 -0800

    switch from nvimtree to oil.nvim

---
+++ lua/main/plugins/oil.lua
@@ -0,0 +1,19 @@
+return {
+    {
+        'stevearc/oil.nvim',
+        ---@module 'oil'
+        ---@type oil.SetupOpts
+        opts = {
+            default_file_explorer = true,
+            columns = {
+                "icon",
+                -- "permissions",
+                -- "size",
+                -- "mtime",
+            },
+        },
+        -- Optional dependencies
+        -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
+        dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+    }
+}
